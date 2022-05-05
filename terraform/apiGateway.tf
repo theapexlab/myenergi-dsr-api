51154f5e633942350a83a6915ecec212af94b3ce
Máté Szlávik <mate@apexlab.io>
2022-05-05 12:32:27 +0200

    fix: api gateway route keys

--- terraform/apiGateway.tf
+++ terraform/apiGateway.tf
@@ -29,13 +29,13 @@ resource "aws_apigatewayv2_stage" "lambda" {
 
 resource "aws_apigatewayv2_route" "aggregator" {
   api_id    = aws_apigatewayv2_api.lambda.id
-  route_key = "ANY /aggregator"
+  route_key = "ANY /aggregator/{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.aggregator.id}"
 }
 
 resource "aws_apigatewayv2_route" "admin" {
   api_id    = aws_apigatewayv2_api.lambda.id
-  route_key = "ANY /admin"
+  route_key = "ANY /admin/{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.admin.id}"
 }
 
